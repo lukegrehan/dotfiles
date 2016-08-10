690b88a60489b13bc7e5c8d8e4752b9b3224f136
Luke Grehan <luketenant@gmail.com>
2016-08-10 14:07:51 +0100

    add run program prompt

--- rc.lua
+++ rc.lua
@@ -234,6 +234,7 @@ globalkeys = awful.util.table.join(
    -- Prompt
     keydoc.group("Misc"),
     awful.key({ modkey }, "r", function () awful.util.spawn(browser) end, "Run browser"),
+    awful.key({ modkey, "Shift" }, "r", function () awful.util.spawn("dmenu_run") end, "Run program"),
     awful.key({ modkey }, "p", function () awful.util.spawn("passmenu") end, "Run passmenu"),
     awful.key({ }, "XF86AudioRaiseVolume", function ()
       awful.util.spawn("amixer set Master 5%+",false) end),
